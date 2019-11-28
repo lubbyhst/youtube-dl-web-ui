214eb56447ed8a9745f78e565317b84610a7e259
lubby <lubby@9f4.de>
2019-11-28 21:56:31 +0100

    added new format for audi mmi

--- app/src/main/java/com/github/lubbyhst/youtubedlwebui/enums/FormatOptions.java
+++ app/src/main/java/com/github/lubbyhst/youtubedlwebui/enums/FormatOptions.java
@@ -2,8 +2,9 @@ package com.github.lubbyhst.youtubedlwebui.enums;
 
 public enum FormatOptions {
     VIDEO_HD("bestvideo[height<=1080]+bestaudio/best[height<=1080][ext=mp4]", "Video 1080p mp4"),
+    VIDEO_MMI("bestvideo[height<=576]+bestaudio/best[height<=576][ext=mp4] --recode-video mp4 --prefer-ffmpeg", "Video MMI special"),
     VIDEO_SD("bestvideo[height<=480]+bestaudio/best[height<=480][ext=mp4]", "Video 480p mp4"),
-    AUDIO_ONLY("bestaudio[ext=m4a]", "Audio m4a");
+    AUDIO_ONLY("bestaudio[ext=mp3]", "Audio mp3");
 
     private final String format;
     private final String displayedName;
