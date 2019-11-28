32529f841afd945fda9950adad1ff1a781b6e69a
lubby <lubby@9f4.de>
2019-11-28 22:21:45 +0100

    changed format options

--- app/src/main/java/com/github/lubbyhst/youtubedlwebui/enums/FormatOptions.java
+++ app/src/main/java/com/github/lubbyhst/youtubedlwebui/enums/FormatOptions.java
@@ -1,10 +1,10 @@
 package com.github.lubbyhst.youtubedlwebui.enums;
 
 public enum FormatOptions {
-    VIDEO_HD("bestvideo[height<=1080]+bestaudio/best[height<=1080] --merge-output-format mp4", "Video 1080p mp4"),
-    VIDEO_MMI("bestvideo[height<=576]+bestaudio/best[height<=576] --merge-output-format mp4 --recode-video mp4 --prefer-ffmpeg", "Video MMI special"),
-    VIDEO_SD("bestvideo[height<=480]+bestaudio/best[height<=480] --merge-output-format mp4", "Video 480p mp4"),
-    AUDIO_ONLY("bestaudio --extract-audio --audio-format mp3", "Audio mp3");
+    VIDEO_HD("[height<=1080] --merge-output-format mp4", "Video 1080p mp4"),
+    VIDEO_MMI("[height<=576] --merge-output-format mp4 --recode-video mp4 --prefer-ffmpeg", "Video MMI special"),
+    VIDEO_SD("[height<=480] --merge-output-format mp4", "Video 480p mp4"),
+    AUDIO_ONLY("--extract-audio --audio-format mp3", "Audio mp3");
 
     private final String format;
     private final String displayedName;
